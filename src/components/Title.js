05ba5e4de7dc8b40cd1d871c12abe143d3483cd8
Mariusz Mycek <m.mycek83@gmail.com>
2019-03-30 18:49:34 +0100

    Add TodoList component

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,7 +8,7 @@ const Title = props => {
       : style.taskQuantity;
   const yet = props.data.length == 0 ? "yet..." : "It's time Todo something!";
   return (
-    <div>
+    <div className={style.header}>
       <h1>Todo app</h1>
       <p>
         You have <span className={taskQuantity}>{props.data.length}</span>{" "}
